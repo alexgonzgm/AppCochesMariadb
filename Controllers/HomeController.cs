90b56d64e93328c4648ff3ad1d6a90e610a19368
Alex <alexgonzgm@gmail.com>
2021-04-30 11:43:56 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using AppCochesMariadb.Data;
+using AppCochesMariadb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCochesMariadb.Controllers
+{
+    public class HomeController : Controller
+    {
+        CochesContext context;
+        public HomeController(CochesContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(this.context.Coches.ToList()); 
+        }
+
+      
+    }
+}
---
+++ Data/CochesContext.cs
@@ -0,0 +1,18 @@
+﻿using ExamenMvcCoreAlexGM.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCochesMariadb.Data
+{
+    public class CochesContext : DbContext
+    {
+        public CochesContext(DbContextOptions<CochesContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Coche> Coches { get; set; }
+    }
+}
